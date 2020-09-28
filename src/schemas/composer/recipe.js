70545df5e617a7732c2b462a9dcabd46f8447665
Cò Bâu <codaubo97@gmail.com>
2020-09-28 17:56:24 +0700

    update recipe

--- src/schemas/composer/recipe.js
+++ src/schemas/composer/recipe.js
@@ -2,10 +2,13 @@ import composeWithMongoose from 'graphql-compose-mongoose'
 import composeWithDataLoader from '../../vendor/graphql-compose-dataloader'
 // models
 import models from '../../models'
+// composer
+import composer from '../composer'
 // constants
 import { CACHE_EXPIRATION } from '../../constants/cache'
 // options
 import { customizationOptions } from '../customizationOptions'
+import { RESOLVER_FIND_BY_ID } from '../../constants/resolver'
 
 export const RecipeTC = composeWithDataLoader(
   composeWithMongoose(models.Recipe, customizationOptions),
@@ -13,5 +16,13 @@ export const RecipeTC = composeWithDataLoader(
     cacheExpiration: CACHE_EXPIRATION
   }
 )
+
+RecipeTC.addRelation('category', {
+  resolver: () => composer.CategoryTC.getResolver(RESOLVER_FIND_BY_ID),
+  prepareArgs: {
+    _id: (source) => source.category
+  },
+  projection: { category: 1 }
+})
 
 export default RecipeTC
