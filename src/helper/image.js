67e729373795b819b66348924501a7371afa0e93
bau <codaubo>
2020-09-30 18:13:44 +0700

    update upload image

--- src/helper/image.js
+++ src/helper/image.js
@@ -1,58 +1,57 @@
+// libs
+import dotenv from 'dotenv'
 const fs = require('fs')
 const mime = require('mime')
 const sharp = require('sharp')
-const AWS = require('aws-sdk')
 const https = require('https')
-
-import { PRODUCT_PATH, RECIPE_PATH, UPLOAD_TYPE } from '../constants/image'
+// composers
+import composer from '../schemas/composer'
+// constants
+import { 
+  PRODUCT_UPLOAD_PATH, 
+  BANNER_UPLOAD_PATH, 
+  RECIPE_UPLOAD_PATH, 
+  UPLOAD_TYPE 
+} from '../constants/image'
+import { RESOLVER_CREATE_ONE } from '../constants/resolver'
+// helpers
 import stringHelper from '../extension/string'
 
-const s3 = new AWS.S3()
+dotenv.config()
 
 const generatePathFileByType = (type) => {
   switch (type) {
     case UPLOAD_TYPE.PRODUCT:
-      return PRODUCT_PATH
+      return PRODUCT_UPLOAD_PATH
     case UPLOAD_TYPE.RECIPE:
-      return RECIPE_PATH
+      return RECIPE_UPLOAD_PATH
     case UPLOAD_TYPE.BANNER:
-      return BANNER_PATH
+      return BANNER_UPLOAD_PATH
     default:
-      return PRODUCT_PATH
+      return PRODUCT_UPLOAD_PATH
   }
 }
 
 const uploadImageAWS = async (file, type) => {
-  console.log('file', file)
-  console.log('type', type)
   try {
     // get path file
-    const pathFile = generatePathFileByType(type)
     const oldFilename = stringHelper.generateRandomStringWithUppercase(file.originalname.length)
     const newFileName = `${Date.now()}-${oldFilename}`
-    // init params aws s3
-    const params = {
-      ACL: 'public-read',
-      Body: file.buffer,
-      Bucket: process.env.REACT_APP_BUCKET_NAME,
-      ContentType: file.mimetype,
-      Key: `${pathFile}/${newFileName}`,
-    }
     // get file extension from fileType
     const fileExtension = mime.getExtension(file.mimetype)
     // upload server AWS
-    const fileUploadAWS = await s3.upload(params).promise()
+    // const fileUploadAWS = await s3.upload(params).promise()
     // create object file to db
     const dataClause = {
-      filename: newFileName,
+      filename: file.filename,
       mimetype: file.mimetype,
       fileExtension: fileExtension,
       encoding: file.encoding,
       fileType: type,
-      originalUrl: fileUploadAWS.Location,
+      // originalUrl: fileUploadAWS.Location,
+      originalUrl: `${process.env.BACK_END_URL_IMAGE}/${file.path.split('\\').join(`/`)}`,
       contentType: file.mimetype.toString(),
     }
-
     // return await models.File.create()
 
     const fileRecord = await composer.FileTC.getResolver(
@@ -75,7 +74,6 @@ const uploadImageAWS = async (file, type) => {
  * @param {*} res
  */
 const uploadMultipleFiles = async (req, res) => {
-  console.log('.req.headers.uploadtype', req.headers.uploadtype)
   const uploadFiles = req.files || []
   const uploadType = req.headers.uploadtype
   const uploadFilePromises = await Promise.all(
--- src/index.js
+++ src/index.js
@@ -1,13 +1,63 @@
+// libs
 import dotenv from 'dotenv'
 import express from "express";
 import mongoose from "mongoose";
 import server from './apollo-server'
-
+// database
 import './mongoose'
+// helpers 
+import imageHelper from './helper/image'
 
-dotenv.config()
 const app = express()
+dotenv.config()
 
+// image
+var multer = require('multer')
+var fs = require('fs-extra')
+var cors = require('cors')
+
+
+// Define port for app to listen on
+const port =  process.env.PORT || 4000;
+
+/* ==================================================== */
+/* =====                                          ===== */
+/* =====       Define folder storage image        ===== */
+/* =====                                          ===== */
+/* ==================================================== */
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    const disk = imageHelper.generatePathFileByType(req.headers.uploadtype)
+    const path = `./${disk}`
+    fs.mkdirsSync(path)
+    cb(null, path)
+  },
+  filename: function (req, file, cb) {
+    cb(null, `${Date.now()}-${file.originalname}`)
+  }
+})
+
+var upload = false
+  ? multer().array('file')
+  : multer({ storage: storage }).array('file')
+
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(cors())
+
+/* ==================================================== */
+/* =====                                          ===== */
+/* =====           Define upload folder           ===== */
+/* =====                                          ===== */
+/* ==================================================== */
+app.use('/uploads', express.static('uploads'))
+ 
+/* ==================================================== */
+/* =====                                          ===== */
+/* =====              Making Routes               ===== */
+/* =====                                          ===== */
+/* ==================================================== */
 app.post('/uploads', (req, res) => {
   upload(req, res, function async(err) {
     if (err instanceof multer.MulterError) {
@@ -18,11 +68,16 @@ app.post('/uploads', (req, res) => {
       // An unknown error occurred when uploading.
     }
     //check
-    uploadMultipleFiles(req, res)
+    imageHelper.uploadMultipleFiles(req, res)
     // Everything went fine.
   })
 })
 
+/* ==================================================== */
+/* =====                                          ===== */
+/* =====       Configure express middlewares      ===== */
+/* =====                                          ===== */
+/* ==================================================== */
 server.applyMiddleware({
   app,
   path: '/',
@@ -40,6 +95,8 @@ server.applyMiddleware({
     })
 })
 
-app.listen({ port: process.env.PORT }, () =>
-  console.log(`🚀 Server ready at http://localhost:${process.env.PORT}`)
+
+// To make the server live
+app.listen({ port: port }, () =>
+  console.log(`🚀 Server ready on port ${port}`)
 );
