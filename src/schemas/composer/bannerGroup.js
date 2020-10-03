75509d6f463786a4fce186f71fa06f03b7c4d6ce
HongDuong <hongduong@HongDuongs-MacBook-Pro.local>
2020-10-03 23:33:32 +0700

    fix banner

--- src/schemas/composer/bannerGroup.js
+++ src/schemas/composer/bannerGroup.js
@@ -6,6 +6,8 @@ import models from '../../models'
 import { CACHE_EXPIRATION } from '../../constants/cache'
 // options
 import { customizationOptions } from '../customizationOptions'
+import BannerTC from './banner'
+import { RESOLVER_BANNER_FIND_MANY } from '../../constants/resolver'
 
 export const BannerGroupTC = composeWithDataLoader(
   composeWithMongoose(models.BannerGroup, customizationOptions),
@@ -13,5 +15,25 @@ export const BannerGroupTC = composeWithDataLoader(
     cacheExpiration: CACHE_EXPIRATION
   }
 )
+
+BannerGroupTC.addFields({
+  banner: {
+    type: [BannerTC],
+    args: BannerTC.getResolver(RESOLVER_BANNER_FIND_MANY).getArgs(),
+    resolve: async (source, args, context, info) => {
+      const rawQuery = {
+        bannerGroup: source._id
+      }
+      return BannerTC.getResolver(RESOLVER_BANNER_FIND_MANY).resolve({
+        source,
+        args,
+        context,
+        info,
+        rawQuery
+      })
+    }
+  }
+})
+
 
 export default BannerGroupTC
---
+++ src/schemas/queries/homePageFE.js
@@ -0,0 +1,41 @@
+// composer
+import composer from '../composer'
+// constant
+import {
+  RESOLVER_FIND_BY_ID,
+  RESOLVER_FIND_MANY
+} from '../../constants/resolver'
+// helper
+import { pageHelper } from '../../models/extensions'
+
+
+export default {
+  getBannerHomePage: {
+    type: composer.BannerGroupTC,
+    args: {
+      where: 'JSON'
+    },
+    resolve: async (_, { where }, context, info) => {
+      // query
+      try {
+        // 1. get config home page
+        const config = await pageHelper.getConfigHomePage()
+
+        if (!config || !config.configBanner) {
+          return null
+        }
+
+        // 2. get banner by config
+        return await composer.BannerGroupTC.getResolver(
+          RESOLVER_FIND_BY_ID
+        ).resolve({
+          args: {
+            _id: config.configBanner.key
+          }
+        })
+      } catch (error) {
+        throw new Error(error)
+      }
+    }
+  },
+}
