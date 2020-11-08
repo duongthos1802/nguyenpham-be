15ada8589b056be2d3251b9e8b8f5ba649357a01
HongDuong <hongduong@HongDuongs-MacBook-Pro.local>
2020-11-09 01:48:44 +0700

    add banner service

--- src/schemas/queries/banner.js
+++ src/schemas/queries/banner.js
@@ -8,8 +8,10 @@ import {
   RESOLVER_CONNECTION,
   RESOLVER_COUNT,
   RESOLVER_BANNER_FIND_MANY,
-  RESOLVER_BANNER_COUNT
+  RESOLVER_BANNER_COUNT,
+  RESOLVER_FIND_ONE
 } from '../../constants/resolver'
+import { CATEGORY_STATUS } from '../../constants/enum'
 
 const BannerTC = composer.BannerTC
 
@@ -18,5 +20,24 @@ export default {
   banners: BannerTC.getResolver(RESOLVER_BANNER_FIND_MANY),
   bannersConnection: BannerTC.getResolver(RESOLVER_CONNECTION),
   bannersCount: BannerTC.getResolver(RESOLVER_BANNER_COUNT),
-  bannersPagination: BannerTC.getResolver(RESOLVER_PAGINATION)
+  bannersPagination: BannerTC.getResolver(RESOLVER_PAGINATION),
+  bannerService: {
+    type: composer.CategoryTC,
+    args: {
+      where: 'JSON'
+    },
+    resolve: async (_, { where }, context, info) => {
+      const { slug } = where
+      return await composer.CategoryTC.getResolver(
+        RESOLVER_FIND_ONE
+      ).resolve({
+        args: {
+          filter: {
+            slug: slug,
+            status: CATEGORY_STATUS.PUBLISHED
+          }
+        }
+      })
+    }
+  }
 }
