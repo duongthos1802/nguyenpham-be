97e29a82c7ff03bac255c35f188ef89c2cd11626
duongth <duongth>
2026-04-04 01:17:03 +0700

    update

--- src/schemas/queries/homePageFE.js
+++ src/schemas/queries/homePageFE.js
@@ -1,38 +1,51 @@
 // composer
-import composer from '../composer'
+import composer from "../composer";
 // constant
 import {
   RESOLVER_FIND_BY_ID,
   RESOLVER_FIND_MANY,
-  RESOLVER_FIND_ONE
-} from '../../constants/resolver'
+  RESOLVER_FIND_ONE,
+} from "../../constants/resolver";
 // helper
-import { pageHelper } from '../../models/extensions'
-import { CATEGORY_FEATURE_COUNT, PRODUCT_IN_CATEGORY_FEATURE_COUNT, RECIPE_IN_CATEGORY_FEATURE_COUNT } from '../../constants'
-import CategoryTC from '../composer/category'
-import { PRODUCT_STATUS, RECIPE_STATUS, CATEGORY_OPTION, CATEGORY_STATUS, EVENT_HOME_BY } from '../../constants/enum'
+import { pageHelper } from "../../models/extensions";
+import {
+  CATEGORY_FEATURE_COUNT,
+  PRODUCT_IN_CATEGORY_FEATURE_COUNT,
+  RECIPE_IN_CATEGORY_FEATURE_COUNT,
+} from "../../constants";
+import CategoryTC from "../composer/category";
+import {
+  PRODUCT_STATUS,
+  RECIPE_STATUS,
+  CATEGORY_OPTION,
+  CATEGORY_STATUS,
+  EVENT_HOME_BY,
+} from "../../constants/enum";
 
 const getIdByVideo = (url) => {
-  if (!url) return null
-  const arrayStringUrl = url.split('=')
-  const id = arrayStringUrl.slice(arrayStringUrl.length - 1, arrayStringUrl.length)
-  return id
-}
+  if (!url) return null;
+  const arrayStringUrl = url.split("=");
+  const id = arrayStringUrl.slice(
+    arrayStringUrl.length - 1,
+    arrayStringUrl.length
+  );
+  return id;
+};
 
 export default {
   getBannerHomePage: {
     type: composer.BannerGroupTC,
     args: {
-      where: 'JSON'
+      where: "JSON",
     },
     resolve: async (_, { where }, context, info) => {
       // query
       try {
         // 1. get config home page
-        const config = await pageHelper.getConfigHomePage()
+        const config = await pageHelper.getConfigHomePage();
 
         if (!config || !config.configBanner) {
-          return null
+          return null;
         }
 
         // 2. get banner by config
@@ -40,28 +53,28 @@ export default {
           RESOLVER_FIND_BY_ID
         ).resolve({
           args: {
-            _id: config.configBanner.key
-          }
-        })
+            _id: config.configBanner.key,
+          },
+        });
       } catch (error) {
-        throw new Error(error)
+        throw new Error(error);
       }
-    }
+    },
   },
 
   getServiceHomePage: {
     type: composer.HtmlBlockGroupTC,
     args: {
-      where: 'JSON'
+      where: "JSON",
     },
     resolve: async (_, { where }, context, info) => {
       // query
       try {
         // 1. get config home page
-        const config = await pageHelper.getConfigHomePage()
+        const config = await pageHelper.getConfigHomePage();
 
         if (!config || !config.configService) {
-          return null
+          return null;
         }
 
         // 2. get service by config
@@ -69,19 +82,19 @@ export default {
           RESOLVER_FIND_BY_ID
         ).resolve({
           args: {
-            _id: config.configService.key
-          }
-        })
+            _id: config.configService.key,
+          },
+        });
       } catch (error) {
-        throw new Error(error)
+        throw new Error(error);
       }
-    }
+    },
   },
 
   getEventHomePage: {
     type: composer.EventHomeTC,
     args: {
-      where: 'JSON'
+      where: "JSON",
     },
     resolve: async (_, { where }, context, info) => {
       // query
@@ -90,18 +103,22 @@ export default {
           eventLeft: null,
           urlVideo: null,
           eventRightActive: null,
-          bannerGroup: null
-        }
+          bannerGroup: null,
+        };
         // 1. get config home page
-        const config = await pageHelper.getConfigHomePage()
+        const config = await pageHelper.getConfigHomePage();
 
         // if (!config && !config.configEventLeft && !config.configEventRight) {
         //   return null
         // }
 
         if (config) {
-
-          const { configEventLeft, configEventRightVideo, configEventRightBanner, configEventRightActive } = config
+          const {
+            configEventLeft,
+            configEventRightVideo,
+            configEventRightBanner,
+            configEventRightActive,
+          } = config;
 
           // 2. get event by config
           // let eventLeft = null
@@ -110,126 +127,126 @@ export default {
               RESOLVER_FIND_BY_ID
             ).resolve({
               args: {
-                _id: configEventLeft.key
-              }
-            })
+                _id: configEventLeft.key,
+              },
+            });
           }
 
           if (configEventRightActive) {
-            event.eventRightActive = configEventRightActive
+            event.eventRightActive = configEventRightActive;
 
             // get Video by config
-            if (configEventRightVideo && configEventRightActive === EVENT_HOME_BY.VIDEO) {
-              event.urlVideo = configEventRightVideo
+            if (
+              configEventRightVideo &&
+              configEventRightActive === EVENT_HOME_BY.VIDEO
+            ) {
+              event.urlVideo = configEventRightVideo;
             }
 
             // get banner by config
-            if (configEventRightBanner && configEventRightActive === EVENT_HOME_BY.BANNER) {
+            if (
+              configEventRightBanner &&
+              configEventRightActive === EVENT_HOME_BY.BANNER
+            ) {
               event.bannerGroup = await composer.BannerGroupTC.getResolver(
                 RESOLVER_FIND_BY_ID
               ).resolve({
                 args: {
-                  _id: config.configEventRightBanner.key
-                }
-              })
+                  _id: config.configEventRightBanner.key,
+                },
+              });
             }
           }
         }
-        return event
-
+        return event;
       } catch (error) {
-        throw new Error(error)
+        throw new Error(error);
       }
-    }
+    },
   },
   categoryFeatures: {
     type: [composer.CategoryFeatureTC],
-    args: {
-
-    },
+    args: {},
     resolve: async (_, args, context, info) => {
       // query
       try {
         // get config home page
-        const config = await pageHelper.getConfigHomePage()
+        const config = await pageHelper.getConfigHomePage();
 
-        const whereClause = {}
-        let idWhereClause = []
+        const whereClause = {};
+        let idWhereClause = [];
         if (config) {
           const {
             // configCategory,
             configCategorySecond,
             configCategoryThird,
+            configCategoryFour,
+          } = config;
+
+          if (
+            configCategorySecond ||
+            configCategoryThird ||
             configCategoryFour
-          } = config
-
-
-
-          if (configCategorySecond || configCategoryThird || configCategoryFour) {
-
+          ) {
             if (configCategorySecond && configCategorySecond.key) {
-              idWhereClause.push(configCategorySecond.key)
+              idWhereClause.push(configCategorySecond.key);
             }
 
             if (configCategoryThird && configCategoryThird.key) {
-              idWhereClause.push(configCategoryThird.key)
+              idWhereClause.push(configCategoryThird.key);
             }
 
             if (configCategoryFour && configCategoryFour.key) {
-              idWhereClause.push(configCategoryFour.key)
+              idWhereClause.push(configCategoryFour.key);
             }
-
           }
         }
 
         // 1. get first 2 category
-        let categories = []
-        let indexId = 0
+        let categories = [];
+        let indexId = 0;
 
         while (indexId < idWhereClause.length) {
-          const cate = await CategoryTC.getResolver(
-            RESOLVER_FIND_ONE
-          ).resolve({
+          const cate = await CategoryTC.getResolver(RESOLVER_FIND_ONE).resolve({
             args: {
               filter: {
-                _id: idWhereClause[indexId]
+                _id: idWhereClause[indexId],
               },
               status: CATEGORY_STATUS.PUBLISHED,
               limit: CATEGORY_FEATURE_COUNT,
-            }
-          })
+            },
+          });
 
           if (cate) {
-            categories.push(cate)
+            categories.push(cate);
           }
-          indexId++
+          indexId++;
         }
 
         if (!categories || categories.length === 0) {
-          return []
+          return [];
         }
 
         // 2. get category info
-        let resultData = []
-        let index = 0
+        let resultData = [];
+        let index = 0;
 
         while (index < categories.length) {
-
           // 2.1. get category name
           let productFilter = {
             OR: [
               {
-                status: PRODUCT_STATUS.PUBLISHED
-              }
+                status: PRODUCT_STATUS.PUBLISHED,
+              },
             ],
-            isPriority: true
-          }
+            isPriority: true,
+          };
 
-          productFilter.category = categories[index]._id
-          const categoryName = categories[index].name
+          productFilter.category = categories[index]._id;
+          const categoryName = categories[index].name;
 
-          let listProducts = []
-          let listRecipes = []
+          let listProducts = [];
+          let listRecipes = [];
 
           if (categories[index].option === CATEGORY_OPTION.PRODUCT) {
             listProducts = await composer.ProductTC.getResolver(
@@ -239,8 +256,8 @@ export default {
                 filter: productFilter,
                 limit: PRODUCT_IN_CATEGORY_FEATURE_COUNT,
                 // sort: args.orderBy
-              }
-            })
+              },
+            });
           }
 
           if (categories[index] === CATEGORY_OPTION.RECIPE) {
@@ -250,11 +267,11 @@ export default {
               args: {
                 filter: {
                   category: productFilter,
-                  status: RECIPE_STATUS.PUBLISHED
+                  status: RECIPE_STATUS.PUBLISHED,
                 },
-                limit: RECIPE_IN_CATEGORY_FEATURE_COUNT
-              }
-            })
+                limit: RECIPE_IN_CATEGORY_FEATURE_COUNT,
+              },
+            });
           }
 
           resultData.push({
@@ -265,16 +282,115 @@ export default {
             image: categories[index].image,
             slug: categories[index].slug,
             products: listProducts,
-            recipes: listRecipes
-          })
+            recipes: listRecipes,
+          });
 
-          index++
+          index++;
         }
 
-        return resultData
+        return resultData;
       } catch (error) {
-        throw new Error(error)
+        throw new Error(error);
       }
-    }
+    },
   },
-}
+  brandFeatures: {
+    type: [composer.CategoryFeatureTC],
+    args: {},
+    resolve: async (_, args, context, info) => {
+      // query
+      try {
+        // get config home page
+        const config = await pageHelper.getConfigHomePage();
+
+        let idWhereClause = [];
+        if (config) {
+          const {
+            configBrand1,
+            configBrand2,
+            configBrand3,
+            configBrand4,
+            configBrand5,
+            configBrand6,
+          } = config;
+
+          if (
+            configBrand1 ||
+            configBrand2 ||
+            configBrand3 ||
+            configBrand4 ||
+            configBrand5 ||
+            configBrand6
+          ) {
+            if (configBrand1 && configBrand1.key) {
+              idWhereClause.push(configBrand1.key);
+            }
+            if (configBrand2 && configBrand2.key) {
+              idWhereClause.push(configBrand2.key);
+            }
+            if (configBrand3 && configBrand3.key) {
+              idWhereClause.push(configBrand3.key);
+            }
+            if (configBrand4 && configBrand4.key) {
+              idWhereClause.push(configBrand4.key);
+            }
+
+            if (configBrand5 && configBrand5.key) {
+              idWhereClause.push(configBrand5.key);
+            }
+
+            if (configBrand6 && configBrand6.key) {
+              idWhereClause.push(configBrand6.key);
+            }
+          }
+        }
+
+        // 1. get first 2 category
+        let categories = [];
+        let indexId = 0;
+
+        while (indexId < idWhereClause.length) {
+          const cate = await CategoryTC.getResolver(RESOLVER_FIND_ONE).resolve({
+            args: {
+              filter: {
+                _id: idWhereClause[indexId],
+              },
+              status: CATEGORY_STATUS.PUBLISHED,
+              limit: CATEGORY_FEATURE_COUNT,
+            },
+          });
+
+          if (cate) {
+            categories.push(cate);
+          }
+          indexId++;
+        }
+
+        if (!categories || categories.length === 0) {
+          return [];
+        }
+
+        // 2. get category info
+        let resultData = [];
+        let index = 0;
+
+        while (index < categories.length) {
+          resultData.push({
+            _id: categories[index]._id,
+            index: categories[index].index,
+            name: categoryName || null,
+            option: categories[index].option,
+            image: categories[index].image,
+            slug: categories[index].slug,
+          });
+
+          index++;
+        }
+
+        return resultData;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+  },
+};
